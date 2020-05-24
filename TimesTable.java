1b3f03db843d603605814f84d657dd40c05ca019
murat-es <54726749+murat-es@users.noreply.github.com>
2020-05-25 01:07:56 +0300

    times table with for

---
+++ TimesTable.java
@@ -0,0 +1,10 @@
+public class TimesTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            for (int j = 1; j <=10; j++) {
+                System.out.print(i +" x "+j + " = " +i*j +"\t\t");
+            }
+            System.out.println();
+        }
+    }
+}
