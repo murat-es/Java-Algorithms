908ed6d288db4fd1c2f3bc7253585119c8827040
Murat Eş <54726749+murat-es@users.noreply.github.com>
2021-02-21 18:55:09 +0300

    Update CalculateTimeAndDay.java

--- CalculateTimeAndDay.java
+++ CalculateTimeAndDay.java
@@ -5,49 +5,50 @@ public class CalculateTimeAndDay {
         Scanner input=new Scanner(System.in);
         System.out.println("To calculate for hours --> 1\n" +
                 "To calculate for days --> 2");
-        int islem=input.nextInt();
-        if (islem==2) {
-            System.out.print("Bugün haftanın kaçıncı günü? ");
+        int operation=input.nextInt();
+        if (operation==2) {
+            System.out.print("What day of the week today?  ");
+            System.out.print("Please enter a value from 1 to 7. (eg 1 for Monday) ");
             int today = input.nextInt();
             while (1 > today || today > 7) {
-                System.err.print("Lütfen 1'den 7'ye kadar bir değer giriniz.(örn:pazartesi için 1) ");
+                System.err.print("Please enter a value from 1 to 7. (eg 1 for Monday) ");
                 today = input.nextInt();
             }
-            System.out.print("Kaç gün sonraki günü merak ediyorsun? ");
+            System.out.print("How many days do you wonder the next day? ");
             int x = input.nextInt();
 
-            String[] gunler = {"Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"};
+            String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
             int a = x % 7;
             today = (today + a) % 7;
 
-            System.out.println(x + " gün sonra günlerden " + gunler[today]);
+            System.out.println(x + " days later it's " + days[today]);
         }
-        else if (islem==1){
+        else if (operation==1){
             System.out.println("What time is it now?");
             System.out.print("Enter the hour : ");
-            int saat=input.nextInt();
+            int hour=input.nextInt();
             System.out.print("Enter the minute : ");
-            int dakika=input.nextInt();
+            int minute=input.nextInt();
 
-            System.out.print("Kaç dakika sonrayı hesaplamak istersiniz? ");
-            int hesapla=input.nextInt();
+            System.out.print("How many minutes would you like to calculate?");
+            int calculate=input.nextInt();
 
-            saat+=hesapla/60;
-            dakika+=hesapla%60;
+            hour+=calculate/60;
+            minute+=calculate%60;
 
-            if (dakika>59){
-                saat+=1;
-                dakika=dakika-60;
+            if (minute>59){
+                hour+=1;
+                minute=minute-60;
             }
-            if (saat>23){
-                saat=saat%24;
+            if (hour>23){
+                hour=hour%24;
             }
-            System.out.println(hesapla+" dakika sonra saat "+saat+":"+dakika);
+            System.out.println("After "+calculate+" minutes, hour --> "+hour+":"+dakika);
         }
         else {
-            System.err.println("Geçersiz sayı girdiniz.");
-            System.out.println("Programdan çıkılıyor...");
+            System.err.println("You entered an invalid number. ");
+            System.out.println("Exiting...");
         }
     }
 }
